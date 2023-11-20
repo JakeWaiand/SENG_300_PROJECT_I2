7e9930ccb275e575a9210b90ac6f3a42baf2471b
JakeWaiand <jacob.waiand@ucalgary.ca>
2023-11-19 21:34:32 -0700

    rewrote part of the printReceipt method

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PrintReceipt.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PrintReceipt.java
@@ -1,4 +1,4 @@
-/*
+/* TODO: add credits for everyone else
  * @author Jake Waiand (30179510)
  */
 package com.thelocalmarketplace.software;
@@ -20,8 +20,12 @@ import com.thelocalmarketplace.hardware.BarcodedProduct;
 import ca.ucalgary.seng300.simulation.InvalidArgumentSimulationException;
 import powerutility.NoPowerException;
 
-public class PrintReceipt implements ReceiptPrinterListener{
+public class PrintReceipt<T> implements ReceiptPrinterListener{
 
+	private ReceiptPrinterBronze printerB = new ReceiptPrinterBronze();
+	private ReceiptPrinterSilver printerS = new ReceiptPrinterSilver();
+	private ReceiptPrinterGold printerG = new ReceiptPrinterGold();
+	
 	public void displayToCustomer(String message) {
 		System.out.println("CUSTOMER: "+message);
 	}
@@ -34,6 +38,16 @@ public class PrintReceipt implements ReceiptPrinterListener{
 		String receipt = "";
 		
 		//make receipt
+		/* This is roughly how the receipt should look:
+		 * TheLocalMarketplace
+		 * 
+		 * RECEIPT DATE: [current date]
+		 * 
+		 * SALE:
+		 * -[list of products]
+		 * 
+		 * TOTAL: [total amount paid]
+		 */
 		LocalDate currentDate = LocalDate.now();
 		receipt += "TheLocalMarketplace\n\nRECEIPT DATE: " + currentDate + "\n\nSALE:\n";
 		for (BarcodedProduct b : bill) {
@@ -41,43 +55,46 @@ public class PrintReceipt implements ReceiptPrinterListener{
 		}
 		receipt += "\nTOTAL: " + amountPaid;
 		
-		//printing the receipt
-		try {
-			switch(printerGrade) {
-				case "BRONZE":
-					ReceiptPrinterBronze printerB = new ReceiptPrinterBronze();
+		//print receipt
+		switch(printerGrade) {
+			case "BRONZE":
+				try {
+					printerB.turnOn();
+					printerB.enable();
 					for (char c : receipt.toCharArray()) {
 						printerB.print(c);
 					}
 					printerB.cutPaper();
 					System.out.println(printerB.removeReceipt());
-				case "GOLD":
-					ReceiptPrinterGold printerG = new ReceiptPrinterGold();
-					for (char c : receipt.toCharArray()) {
-						printerG.print(c);
-					}
-					printerG.cutPaper();
-					System.out.println(printerG.removeReceipt());
-				case "SILVER":
-					ReceiptPrinterSilver printerS = new ReceiptPrinterSilver();
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			case "SILVER":
+				try {
+					printerS.turnOn();
+					printerS.enable();
 					for (char c : receipt.toCharArray()) {
 						printerS.print(c);
 					}
 					printerS.cutPaper();
 					System.out.println(printerS.removeReceipt());
-			}
-		} catch (OverloadedDevice e) {
-			System.out.println(e.getMessage());
-			
-		} catch (EmptyDevice e) {
-			System.out.println(e.getMessage());
-			
-		} catch (NoPowerException e) {
-			System.out.println(e.getMessage());
-			
-		} catch (InvalidArgumentSimulationException e) {
-			System.out.println(e.getMessage());
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			case "GOLD":
+				try {
+					printerG.turnOn();
+					printerG.enable();
+					for (char c : receipt.toCharArray()) {
+						printerG.print(c);
+					}
+					printerG.cutPaper();
+					System.out.println(printerG.removeReceipt());
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
 		}
+		System.out.println("");
 	}
 	
 	@Override
@@ -88,8 +105,7 @@ public class PrintReceipt implements ReceiptPrinterListener{
 
 	@Override
 	public void aDeviceHasBeenDisabled(IDevice<? extends IDeviceListener> device) {
-		// TODO Auto-generated method stub
-		
+		displayToAttendant("The receipt printer is disabled.");
 	}
 
 	@Override
@@ -100,8 +116,7 @@ public class PrintReceipt implements ReceiptPrinterListener{
 
 	@Override
 	public void aDeviceHasBeenTurnedOff(IDevice<? extends IDeviceListener> device) {
-		// TODO Auto-generated method stub
-		
+		displayToAttendant("The receipt printer is off.");
 	}
 
 	@Override
