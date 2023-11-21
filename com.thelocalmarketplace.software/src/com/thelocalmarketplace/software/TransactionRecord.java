4fa2a7c9cea7bb167899686d92081da9fa43fdbb
tiffany-hung <73262451+tiffany-hung@users.noreply.github.com>
2023-11-20 20:05:19 -0700

    Add documentation
    names + UCIDs

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/TransactionRecord.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/TransactionRecord.java
@@ -1,5 +1,12 @@
 /*
- * TODO: add everyone's name + UCID
+ * Favour Adah - 30140479
+ * Farouq Arafeh - 30258214
+ * Adam Attwood - 30185194
+ * Lucas Carvalho - 30113633
+ * Tiffany Hung - 10149429
+ * Mann Patel - 30182233
+ * Arumakankani Sayuru Silva - 30190402
+ * Jake Waiand - 30179510
  * 
  * Description: transaction record class keeps tract of everything on the bill.
  * 
