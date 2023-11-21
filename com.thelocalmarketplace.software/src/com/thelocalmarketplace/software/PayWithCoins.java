ee26874ce8a2b2c21b2db5c36d4ccfbbf5d4d48e
lucascarvalho <lucas.carvalho@ucalgary.ca>
2023-11-20 18:06:23 -0700

    connected the session and pwc class to make payment smooth and work together, major changes made to pwc class

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithCoins.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithCoins.java
@@ -1,55 +1,30 @@
 package com.thelocalmarketplace.software;
 
 import java.math.BigDecimal;
+
+import com.tdc.IComponent;
 import com.tdc.IComponentObserver;
+import com.tdc.coin.CoinValidator;
 import com.tdc.coin.CoinValidatorObserver;
 import com.thelocalmarketplace.hardware.*;
 
 public class PayWithCoins implements CoinValidatorObserver {
-    private SelfCheckoutStation sm;
+    private AbstractSelfCheckoutStation sm;
+    private Session session;
     private static boolean customerSelectedPayCoins = false;
     private static double amountOwed = 0;
-    private String stationType;
 
-    public PayWithCoins(SelfCheckoutStation sm, String stationType) {
-        this.sm = sm;
-        this.stationType = stationType;
+    public PayWithCoins(Session session) {
+        this.sm = session.station;
+        this.session = session;
         this.sm.coinValidator.attach(this);
     }
 
-    @Override
-    public void validCoinDetected(CoinValidator validator, BigDecimal value) {
-        if (amountOwed > 0 && customerSelectedPayCoins && State.isSession()) {
-            amountOwed -= value.doubleValue();
-            displayMessageToScreen(String.format("Amount Due: $%.2f", Math.max(amountOwed, 0)));
-        }
-        if (amountOwed <= 0) {
-            dispenseChange();
-            printReceipt();
-            resetPayment();
-        }
-    }
-
-    @Override
-    public void invalidCoinDetected(CoinValidator validator) {
-        displayMessageToScreen("Invalid coin detected. Please try another coin.");
-    }
-
+    
     private void dispenseChange() {
         double change = Math.abs(amountOwed);
-        if (change > 0) {
-            switch (stationType) {
-                case "Gold":
-                    
-                    break;
-                case "Silver":
-                    
-                    break;
-                case "Bronze":
-                    
-                    break;
-            }
-            displayMessageToScreen(String.format("Dispensing change: $%.2f", change));
+        if (change > 0) {    
+        	displayMessageToScreen(String.format("Dispensing change: $%.2f", change));
         }
     }
 
@@ -74,4 +49,50 @@ public class PayWithCoins implements CoinValidatorObserver {
     public double getAmountOwed() {
         return amountOwed;
     }
+
+	@Override
+	public void enabled(IComponent<? extends IComponentObserver> component) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disabled(IComponent<? extends IComponentObserver> component) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnedOn(IComponent<? extends IComponentObserver> component) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnedOff(IComponent<? extends IComponentObserver> component) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void validCoinDetected(CoinValidator validator, BigDecimal value) {
+		if (amountOwed > 0 && customerSelectedPayCoins && State.isSession()) {
+            amountOwed -= value.doubleValue();
+            displayMessageToScreen(String.format("Amount Due: $%.2f", Math.max(amountOwed, 0)));
+        }
+        if (amountOwed <= 0) {
+            dispenseChange();
+            session.printReceipt();
+            resetPayment();
+        }
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void invalidCoinDetected(CoinValidator validator) {
+		displayMessageToScreen("Invalid coin detected. Please try another coin.");
+		// TODO Auto-generated method stub
+		
+	}
 }
