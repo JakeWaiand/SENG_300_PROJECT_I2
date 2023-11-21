fff64bf1f92fd06a0f24441674943efa7e37a3ae
adahf <adahf@10.0.0.23>
2023-11-20 19:45:16 -0700

    current updated test classes for test for print receipt and add own bags

--- com.thelocalmarketplace.software.test/src/TestAddOwnBags.java
+++ com.thelocalmarketplace.software.test/src/TestAddOwnBags.java
@@ -1,12 +1,59 @@
+import com.thelocalmarketplace.software.AddOwnBag;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.lang.reflect.Method;
 
 public class TestAddOwnBags {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testAddOwnBagsProcess() throws Exception {
+        AddOwnBag checkoutSystem = new AddOwnBag();
 
+        invokePrivateMethod(checkoutSystem, "waitForCustomerInput");
+        invokePrivateMethod(checkoutSystem, "promptToAddBags");
+        invokePrivateMethod(checkoutSystem, "confirmBagsAdded");
+
+        double mockWeight = 4.0;
+        assertEquals(mockWeight, invokePrivateMethodWithReturnValue(checkoutSystem, "checkWeightChange"), 0.01);
+
+        double heavyWeight = 6.0;
+        try {
+            invokePrivateMethod(checkoutSystem, "bagsTooHeavyExtension", heavyWeight);
+            fail("Expected exception not thrown");
+        } catch (Exception e) {
+            // Expected exception, do nothing
+        }
+
+        double normalWeight = 3.0;
+        try {
+            invokePrivateMethod(checkoutSystem, "bagsTooHeavyExtension", normalWeight);
+        } catch (Exception e) {
+            fail("Unexpected exception thrown");
+        }
+
+        invokePrivateMethod(checkoutSystem, "signalToContinue");
+    }
+
+    private void invokePrivateMethod(AddOwnBag instance, String methodName, Object... args) throws Exception {
+        Class<?>[] parameterTypes = new Class[args.length];
+        for (int i = 0; i < args.length; i++) {
+            parameterTypes[i] = args[i].getClass();
+        }
+
+        Method method = AddOwnBag.class.getDeclaredMethod(methodName, parameterTypes);
+        method.setAccessible(true);
+        method.invoke(instance, args);
+    }
+
+    private double invokePrivateMethodWithReturnValue(AddOwnBag instance, String methodName, Object... args) throws Exception {
+        Class<?>[] parameterTypes = new Class[args.length];
+        for (int i = 0; i < args.length; i++) {
+            parameterTypes[i] = args[i].getClass();
+        }
+
+        Method method = AddOwnBag.class.getDeclaredMethod(methodName, parameterTypes);
+        method.setAccessible(true);
+        return (double) method.invoke(instance, args);
+    }
 }
--- com.thelocalmarketplace.software.test/src/TestPrintReceipt.java
+++ com.thelocalmarketplace.software.test/src/TestPrintReceipt.java
@@ -1,12 +1,67 @@
+import com.jjjwelectronics.Numeral;
+import com.jjjwelectronics.scanner.Barcode;
+import com.thelocalmarketplace.hardware.BarcodedProduct;
+import com.thelocalmarketplace.hardware.SelfCheckoutStationBronze;
+import com.thelocalmarketplace.hardware.SelfCheckoutStationSilver;
+import com.thelocalmarketplace.hardware.SelfCheckoutStationGold;
+import com.thelocalmarketplace.software.PrintReceipt;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.ArrayList;
 
 public class TestPrintReceipt {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    private BarcodedProduct barcodedProduct;
+	private Barcode b1;
+	private Barcode b2;
+	private BarcodedProduct barcodedProduct2;
 
+	@Test
+    public void testPrintReceipt() {
+        // Create instances of SelfCheckoutStation for each type (Bronze, Silver, Gold)
+        SelfCheckoutStationBronze bronzeStation = new SelfCheckoutStationBronze();
+        SelfCheckoutStationSilver silverStation = new SelfCheckoutStationSilver();
+        SelfCheckoutStationGold goldStation = new SelfCheckoutStationGold();
+
+        // Create instances of BarcodedProduct for testing
+        b1 = new Barcode(new Numeral[] { Numeral.one });
+        barcodedProduct = new BarcodedProduct(b1, "Apple", 5, 1);
+        b2 = new Barcode(new Numeral[] { Numeral.two });
+        barcodedProduct2 = new BarcodedProduct(b2, "Very Very Small Fruitcake", 2, 1);
+
+        // Create an ArrayList to simulate the bill
+        ArrayList<BarcodedProduct> bill = new ArrayList<>();
+        bill.add(barcodedProduct);
+        bill.add(barcodedProduct2);
+
+        // Test for Bronze Station
+        PrintReceipt printReceiptBronze = new PrintReceipt(bronzeStation);
+        testPrintReceiptForStation(printReceiptBronze, bill);
+
+        // Test for Silver Station
+        PrintReceipt printReceiptSilver = new PrintReceipt(silverStation);
+        testPrintReceiptForStation(printReceiptSilver, bill);
+
+        // Test for Gold Station
+        PrintReceipt printReceiptGold = new PrintReceipt(goldStation);
+        testPrintReceiptForStation(printReceiptGold, bill);
+    }
+
+    private void testPrintReceiptForStation(PrintReceipt printReceipt, ArrayList<BarcodedProduct> bill) {
+        // Mock the display and printer actions
+        printReceipt.displayToCustomer("Testing display to customer");
+        printReceipt.displayToAttendant("Testing display to attendant");
+
+        // Print the receipt and cut it
+        printReceipt.printReceipt(bill, 25.0);
+        printReceipt.cutReceipt();
+
+        // Remove the receipt and verify the output
+        String removedReceipt = printReceipt.removeReceipt();
+        assertEquals("TheLocalMarketplace\n\nRECEIPT DATE:", removedReceipt.substring(0, 36));
+        assertTrue(removedReceipt.contains("Product 1 - 10.0"));
+        assertTrue(removedReceipt.contains("Product 2 - 15.0"));
+        assertTrue(removedReceipt.contains("TOTAL: 25.0"));
+    }
 }
