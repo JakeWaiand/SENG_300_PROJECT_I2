eba345bca1759039754884e62766e46129d6ba4f
Mann Patel <130435633+mannpatel2@users.noreply.github.com>
2023-11-20 19:32:09 -0700

    Update AddOwnBag.java

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/AddOwnBag.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/AddOwnBag.java
@@ -1,10 +1,16 @@
 package com.thelocalmarketplace.software;
 
-public class AddOwnBag{
+public class AddOwnBag {
 
     private static final double ALLOWED_WEIGHT_LIMIT = 5.0; // Adjust as needed
+    private boolean isSystemReady = true; // Placeholder for system readiness check
 
     public void addOwnBagsProcess() {
+        if (!isSystemReady) {
+            System.out.println("System is not ready to note weight discrepancies.");
+            return;
+        }
+
         try {
             waitForCustomerInput();
             promptToAddBags();
@@ -12,49 +18,49 @@ public class AddOwnBag{
             double weight = checkWeightChange();
             if (weight > ALLOWED_WEIGHT_LIMIT) {
                 bagsTooHeavyExtension(weight);
+            } else {
+                signalToContinue();
             }
-            signalToContinue();
         } catch (Exception e) {
             handleExceptions(e);
         }
     }
 
     private void waitForCustomerInput() {
-        // Wait for customer to signal the desire to add their own bags
-        // Implementation depends on the UI system
+        // Implementation: Wait for customer to signal the desire to add their own bags
+        System.out.println("Waiting for customer input...");
     }
 
     private void promptToAddBags() {
-        // Indicate to the customer that they should add their own bags now
-        // Implementation depends on the UI system
+        // Implementation: Indicate to the customer that they should add their own bags now
+        System.out.println("Please add your bags now.");
     }
 
     private void confirmBagsAdded() {
-        // Wait for customer to signal that the bags have been added
-        // Implementation depends on the UI system
+        // Implementation: Wait for customer to signal that the bags have been added
+        System.out.println("Confirming bags have been added...");
     }
 
     private double checkWeightChange() {
-        // Detect the weight change
-        // This method should interface with the weight sensor
-        // Return the detected weight
-        return 0.0; // Placeholder
+        // Implementation: Interface with the weight sensor to detect weight change
+        double detectedWeight = AddItemByBarcodeScanner.expectedWeight;
+        System.out.println("Detected weight change: " + detectedWeight + " kg");
+        return detectedWeight;
     }
 
     private void bagsTooHeavyExtension(double weight) {
         // Handle the case where the added bags are too heavy
-        // Additional logic to be implemented as per requirement
+        System.out.println("Bags are too heavy! Weight: " + weight + " kg");
     }
 
     private void signalToContinue() {
         // Signal to the customer that they may now continue
-        // Implementation depends on the UI system
+        System.out.println("All set! You may continue.");
     }
 
     private void handleExceptions(Exception e) {
-        // Handle any exceptions, such as the system not being ready to note weight discrepancies
+        // Handle any exceptions
         System.out.println("Exception occurred: " + e.getMessage());
-        // Additional exception handling logic as needed
     }
 
     // Main method for demonstration purposes
@@ -65,5 +71,3 @@ public class AddOwnBag{
         System.out.println("AddOwnBag process ended");
     }
 }
-
-
