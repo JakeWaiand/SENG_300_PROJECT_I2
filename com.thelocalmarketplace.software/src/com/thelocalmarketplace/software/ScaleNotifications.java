7aa993c41c51fbb1d0396d956c9ebb71f4b23b1c
tiffany-hung <73262451+tiffany-hung@users.noreply.github.com>
2023-11-20 20:04:17 -0700

    Add documentation
    names + UCIDs

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/ScaleNotifications.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/ScaleNotifications.java
@@ -1,5 +1,12 @@
 /*
- * TODO: add everyone's name + UCID
+ * Favour Adah - 30140479
+ * Farouq Arafeh - 30258214
+ * Adam Attwood - 30185194
+ * Lucas Carvalho - 30113633
+ * Tiffany Hung - 10149429
+ * Mann Patel - 30182233
+ * Arumakankani Sayuru Silva - 30190402
+ * Jake Waiand - 30179510
  * 
  * Description: scale notifications class implements an electronic scale observer.
  * i.e. any events related to the electronic scale gets notified here.
